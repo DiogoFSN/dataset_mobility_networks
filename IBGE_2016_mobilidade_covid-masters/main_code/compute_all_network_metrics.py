97131931d2f22ad1065f2625c3d4e4a02ea17da7
Diogo Freitas <68716441+DiogoFSN@users.noreply.github.com>
2023-06-05 15:14:16 -0300

    add by diogo

---
+++ IBGE_2016_mobilidade_covid-masters/main_code/compute_all_network_metrics.py
@@ -0,0 +1,16 @@
+import os
+
+mydir = os.getcwd()
+print('mydir: ', mydir)
+files = os.listdir(mydir + '\\IBGE_2016_mobilidade_covid-masters\\main_code\\input_data\\networks')
+if files.__len__() == 0:
+    os.system('python \\IBGE_2016_mobilidade_covid-masters\\main_code\\input_data\\create_networks.py')
+files.sort()
+
+# mydir = os.chdir('src/metrics/')
+# print(mydir)
+# mydir = os.getcwd()
+# print(mydir)
+
+print('Computing metrics: ')
+os.system('python src\\plot_network.py')
